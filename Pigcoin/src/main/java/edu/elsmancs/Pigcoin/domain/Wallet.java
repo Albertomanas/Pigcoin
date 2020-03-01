1dc501dcfadba9ac732bf07d39aa232dde874c04
Albertomanas <amanas@cifpfbmoll.eu>
2020-03-01 13:58:47 +0100

    feat(Wallet): generateKeyPair con sus setters y getters

---
+++ Pigcoin/src/main/java/edu/elsmancs/Pigcoin/domain/Wallet.java
@@ -0,0 +1,34 @@
+package edu.elsmancs.Pigcoin.domain;
+
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class Wallet {
+
+    private PublicKey address;
+    private PrivateKey sKey;
+    private double total_input = 0d;
+    private double total_output = 0d;
+    private double balance = 0d;
+
+
+    public void setSK(PrivateKey sKey) {
+        this.sKey = sKey;
+    }
+
+    public void setAddress(PublicKey address) {
+        this.address = address;
+    }
+
+    public PublicKey getAddress() {
+        return this.address;
+    }
+
+    public void generateKeyPair() {
+        KeyPair pair = GenSig.generateKeyPair();
+        this.setSK(pair.getPrivate());
+        this.setAddress(pair.getPublic());
+    }
+}
