251fa7b469d012bfd0c501bdc94feab367b30fe2
Albertomanas <amanas@cifpfbmoll.eu>
2020-03-01 17:05:45 +0100

    test(TransactionTest): comprobarKeys

--- Pigcoin/src/test/java/edu/elsmancs/Pigcoin/domain/TransactionTest.java
+++ Pigcoin/src/test/java/edu/elsmancs/Pigcoin/domain/TransactionTest.java
@@ -1,4 +1,17 @@
 package edu.elsmancs.Pigcoin.domain;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TransactionTest {
+
+     @Test
+     public void comprobarAsignacionKeyTest(){
+         Transaction transaction = new Transaction();
+         Wallet wallet = new Wallet();
+         assertNotNull(wallet);
+         assertNotNull(transaction);
+         assertEquals(transaction.getPkeySender(), wallet.getAddress());
+    }
+
 }
