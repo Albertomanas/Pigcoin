7cff02a4098e7afd6ffa6fc5da9aa4c3d5db2fec
Albertomanas <amanas@cifpfbmoll.eu>
2020-03-02 18:12:44 +0100

    feat(BlockChain): Estructura try catch incompleta

--- Pigcoin/src/main/java/edu/elsmancs/Pigcoin/domain/BlockChain.java
+++ Pigcoin/src/main/java/edu/elsmancs/Pigcoin/domain/BlockChain.java
@@ -2,11 +2,15 @@ package edu.elsmancs.Pigcoin.domain;
 
 import java.security.PublicKey;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BlockChain {
 
     private final List<Transaction> blockChain = new ArrayList<>();
+    private byte[] signedTransaction = null;
+    private final Map<String, Double> consumedCoins = new HashMap<>();
 
     public BlockChain(){
     }
@@ -54,8 +58,50 @@ public class BlockChain {
         }
         double[] pigcoins = {pigcoinsEntrada, pigcoinsSalida};
         return pigcoins;
-
     }
 
+    /** Autentificación de la transacción
+     *  1. processTransactions tiene que cumplir:
+     *         isSignatureValid()
+     *         isConsumedCoinValid()
+     *         si se cumple, createTransaction()
+     *
+     *         Este requisito es parecido a EnZilium
+     */
+
+
+    public boolean isSignatureValid(PublicKey pKey_sender, String message, byte[] signedTransaction) {
+        return GenSig.verify(pKey_sender, message, signedTransaction);
+    }
+
+    public Map<String, Double> getConsumedCoins() {
+        return this.consumedCoins;
+    }
+
+    /**public boolean isConsumedCoinValid(Map<String, Double> consumedCoins) {
+    }
+     */
+
+   private void require(Boolean holds) throws Exception {
+       if (!holds) {
+           throw new Exception();
+       }
+   }
+
+
+   /**public void processTransactions(PublicKey pKey_recipient, Map<String, Double> consumedCoins, byte[] signedTransaction) {
+
+        try {
+            require();
+        } catch (Exception e){}
+   }
+
+   /**public void createTransaction(PublicKey pKey_sender, String message, byte[] signedTransaction){
+
+        try {
+            requite();
+        } catch (Exception e){}
+   }
+    */
 }
 
