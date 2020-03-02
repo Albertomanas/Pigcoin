f3c7356da3a78e66dd8d90aabcf452fcbc94a1aa
Albertomanas <amanas@cifpfbmoll.eu>
2020-03-02 15:22:52 +0100

    refactor(WalletTest): Refactorización valores acorde al main

--- Pigcoin/src/test/java/edu/elsmancs/Pigcoin/domain/WalletTest.java
+++ Pigcoin/src/test/java/edu/elsmancs/Pigcoin/domain/WalletTest.java
@@ -31,24 +31,19 @@ public class WalletTest {
 
         BlockChain bChain = new BlockChain();
         Transaction trx = new Transaction();
-        trx = new Transaction("hash_1", "0", wallet_1.getAddress(), wallet_2.getAddress(), 20, "a flying pig!");
-
-
-        trx = new Transaction("hash_1", "0", origin.getAddress(), wallet_1.getAddress(), 20, "bacon eggs");
+        trx = new Transaction("hash_1", "0", origin.getAddress(), wallet_1.getAddress(), 20, "a flying pig!");
         bChain.addOrigin(trx);
         trx = new Transaction("hash_2", "1", origin.getAddress(), wallet_2.getAddress(), 10, "spam spam spam");
         bChain.addOrigin(trx);
-        trx = new Transaction("hash_3", "hash_1", wallet_1.getAddress(), wallet_2.getAddress(), 20, "a flying pig!");
-        bChain.addOrigin(trx);
 
         wallet_1.loadCoins(bChain);
-        assertEquals(40, wallet_1.getTotalInput(), delta);
-        assertEquals(40, wallet_1.getTotalOutput(), delta);
-        assertEquals(80, wallet_1.getBalance(), delta);
+        assertEquals(20, wallet_1.getTotalInput(), delta);
+        assertEquals(0, wallet_1.getTotalOutput(), delta);
+        assertEquals(20, wallet_1.getBalance(), delta);
 
         wallet_2.loadCoins(bChain);
-        assertEquals(25, wallet_2.getTotalInput(), delta);
+        assertEquals(10, wallet_2.getTotalInput(), delta);
         assertEquals(0, wallet_2.getTotalOutput(), delta);
-        assertEquals(25, wallet_2.getBalance(), delta);
+        assertEquals(10, wallet_2.getBalance(), delta);
     }
 }
