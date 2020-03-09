d7df5f12e3f2240ebb1f5b96a90386f5c6ab16eb
Albertomanas <amanas@cifpfbmoll.eu>
2020-03-09 13:27:42 +0100

    test(BlockChainTest): Caso test comprobarAddOriginTest()

--- Pigcoin/src/test/java/edu/elsmancs/Pigcoin/domain/BlockChainTest.java
+++ Pigcoin/src/test/java/edu/elsmancs/Pigcoin/domain/BlockChainTest.java
@@ -15,11 +15,19 @@ public class BlockChainTest {
         assertEquals(blockChain.getBlockChain().get(0), transaction);
     }
 
-    /**
+
     @Test
     public void comprobarAddOriginTest() {
-        transferencia.addOrigin(Paco.getPkeySender());
+        Wallet origin = new Wallet();
+        origin.generateKeyPair();
+        Wallet wallet_1 = new Wallet();
+        wallet_1.generateKeyPair();
+
+        BlockChain bChain = new BlockChain();
+        Transaction trx = new Transaction();
+        trx = new Transaction("hash_1", "0", origin.getAddress(), wallet_1.getAddress(), 20, "bacon eggs");
+        bChain.addOrigin(trx);
+        assertEquals(20, bChain.getBlockChain().get(0).getPigcoins(), 0);
     }
 
-   */
 }
